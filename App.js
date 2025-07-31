79a050a36bda03f51b47dc7bd34e11502d0cd8de
sansanchez2090 <ssanty2090@gmail.com>
2025-07-31 05:11:50 -0500

    Creación boceto de la app

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { getPopularMovies } from './lib/tbdm';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    getPopularMovies().then((movies) => {
+      setMovies(movies);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+
+      {movies.map((movie) => (
+        <View key={movie.id} style={styles.card}>
+          <Image source={{ uri: movie.image }} style={styles.image} />
+          <Text style={ styles.title }>{movie.title}</Text>
+          <Text style={ styles.description }>{movie.description}</Text>
+          <Text style={ styles.score }>{movie.score}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +31,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  card:{
+    marginBottom: 10,
+  },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  description: {
+    color: '#eee',
+    fontSize: 14,
+    marginBottom: 10,
+  },
+  score: {
+    color: 'green',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margintop: 10,
   },
 });
